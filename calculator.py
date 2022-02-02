d29c16c49b2d76864d4518dfb023800855a0f71a
Jonas <jonas_elmesten@hotmail.com>
2022-02-02 17:21:09 +0100

    Added new method for string add and test.

--- calculator.py
+++ calculator.py
@@ -6,3 +6,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def add_and_return_string(num_1, num_2):
+    return str(num_1) + str(num_2)
--- test/test_calcualtor.py
+++ test/test_calcualtor.py
@@ -8,3 +8,6 @@ class TestCalculator:
 
     def test_sub(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_string_add(self):
+        assert isinstance(calculator.add_and_return_string(1, 2), str)
